08d5029a45e2708d069623f84276ce1c79d3dd9e
Anh Truong <68137985+Apirich@users.noreply.github.com>
2023-09-01 00:14:07 -0700

    Push folder to showcase REACH apprentice

---
+++ chp.js
@@ -0,0 +1,10 @@
+//draw 3rd song name to song list
+//will be upgraded
+function CHP()
+{
+    this.name = "soalive";
+    this.x = 20;
+    this.y = 20;
+    this.width = 20;
+    this.height = 20;
+}
